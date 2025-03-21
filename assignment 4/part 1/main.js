c814ba846922b5876059fa07985766e917bfd101
Zakaria-Ahmed8 <zakaria.ahmed2@dcmail.ca>
2025-03-21 16:30:11 -0400

    Update main.js
    converts weight to stone and temperature to centigrade

--- assignment 4/part 1/main.js
+++ assignment 4/part 1/main.js
@@ -32,8 +32,9 @@ function result() {
   }
  
   if(document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature =  Math.round(94);
+    const weight = Math.round(300 / 14);
+    const temperature =  Math.round((94 - 32) * (5 / 9));
+
 
   }
 
