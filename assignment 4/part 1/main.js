fb4bdae2b1fcf310a716c2bbc9f0f3e10c2f6505
Zakaria-Ahmed8 <zakaria.ahmed2@dcmail.ca>
2025-03-21 16:31:40 -0400

    Update main.js

--- assignment 4/part 1/main.js
+++ assignment 4/part 1/main.js
@@ -32,8 +32,8 @@ function result() {
   }
  
   if(document.getElementById("uk").checked) {
-    const weight = Math.round(300 / 14);
-    const temperature =  Math.round((94 - 32) * (5 / 9));
+    const weight = `${Math.round(300 / 14)} stone`;
+    const temperature =  `${Math.round((94 - 32) * (5 / 9))} centigrade`;
 
 
   }
