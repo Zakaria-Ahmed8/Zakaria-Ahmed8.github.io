13d60a1379ec3cadebd4851222bf947bcddb289c
Zakaria-Ahmed8 <zakaria.ahmed2@dcmail.ca>
2025-03-24 18:55:00 -0400

    Update main.js
    added the collision method to the ball class

--- assignment 4/part 3/main.js
+++ assignment 4/part 3/main.js
@@ -56,6 +56,21 @@ class Ball {
         this.x += this.velX;
         this.y += this.velY;
         }
+    // collision method to change color of the balls if they touch
+    collisionDetect() {
+        for (const ball of balls) {
+            if (this !== ball) {
+            const dx = this.x - ball.x;
+            const dy = this.y - ball.y;
+            const distance = Math.sqrt(dx * dx + dy * dy);
+        
+            if (distance < this.size + ball.size) {
+                ball.color = this.color = randomRGB();
+            }
+            }
+        }
+        }
+
     
         
   }
