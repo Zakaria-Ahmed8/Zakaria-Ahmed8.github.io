6075e840a06ebe008307aeb51296501768e98be6
Zakaria-Ahmed8 <zakaria.ahmed2@dcmail.ca>
2025-03-24 17:04:41 -0400

    Update main.js
    adding array of image filenames and alt text

--- assignment 4/part 2/main.js
+++ assignment 4/part 2/main.js
@@ -1,18 +1,27 @@
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
 
+
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const img = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
 
 /* Declaring the alternative text for each image file */
+const imgAlt = ["Photo 1", "Photo 2", "Photo 3", "Photo 4", "Photo 5"]
 
 /* Looping through images */
-
 const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
+newImage.setAttribute('src', `images/${img[i]}`);
+newImage.setAttribute('alt', imgAlt[i]);
 thumbBar.appendChild(newImage);
 
+
+
+
+
 /* Wiring up the Darken/Lighten button */
+
+
+
